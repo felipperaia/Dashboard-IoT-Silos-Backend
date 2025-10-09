adc8901ffb5b14a94600f2b633d5db92e73a06c2
Felippe Adriel <felippeadriel46@gmail.com>
2025-10-09 14:33:05 -0300

    atualização para vercel

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ logger = logging.getLogger("uvicorn.error")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://dashboard-io-t-silos-front-g8b87imzs.vercel.app/login",
+        "https://plataforma-silos-senac.vercel.app/login",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:5173", 
