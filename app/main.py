d87fb75ff94cd4f601f0f3b7ae59504c31917c0e
Felippe Adriel <felippeadriel46@gmail.com>
2025-10-09 15:31:29 -0300

    att netlify

--- app/main.py
+++ app/main.py
@@ -19,13 +19,8 @@ logger = logging.getLogger("uvicorn.error")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://dashboardsilo.netlify.app/login",
         "https://dashboardsilo.netlify.app",
-        "https://dashboardsilo.netlify.app/dashboard",
-        "https://68e7fb5791a50200082f804f--dashboardsilo.netlify.app/login",
-        "https://68e7fb5791a50200082f804f--dashboardsilo.netlify.app",
         "https://plataforma-silos-senac.vercel.app/",
-        "https://plataforma-silos-senac.vercel.app/login",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:5173", 
